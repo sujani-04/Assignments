cda761102a54a642954b1ec5e012152f327e69e8
sujani-04 <94522136+sujani-04@users.noreply.github.com>
2021-12-04 15:01:45 +0530

    Add files via upload

---
+++ Java Assignments/Day01Assignment/NaturalNumbers.java
@@ -0,0 +1,13 @@
+package com.sonata;
+
+public class NaturalNumbers {
+	public static void main(String args[])
+	{
+		int i; 
+		for( i = 1; i <= 10; i++ )
+		{
+			System.out.println(i); 
+		} 
+	}
+
+}
