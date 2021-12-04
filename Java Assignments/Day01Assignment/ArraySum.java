7ca2a5f9c5ac6aa3727904f55b396a69a5fd35e7
sujani-04 <94522136+sujani-04@users.noreply.github.com>
2021-12-04 14:56:48 +0530

    Update ArraySum.java

--- Java Assignments/Day01Assignment/ArraySum.java
+++ Java Assignments/Day01Assignment/ArraySum.java
@@ -1,1 +1,14 @@
+package com.sonata;
+
+public class ArraySum {
+	public static void main(String args[])
+	{
+		int arr[]= {1,2,3,4,5};
+		int sum=0;
+		for(int i : arr)
+		sum += i;
+		System.out.println("The sum is " + sum);
+	}
+
+}
 
