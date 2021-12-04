a58644a6cde9d68381590021f45f4e3408a43ec4
sujani-04 <94522136+sujani-04@users.noreply.github.com>
2021-12-04 15:02:11 +0530

    Add files via upload

---
+++ Java Assignments/Day01Assignment/ReverseArray.java
@@ -0,0 +1,14 @@
+package com.sonata;
+
+public class ReverseArray {
+	public static void main(String args[])
+	{
+		int [] arr = new int [] {1, 2, 3, 4, 5};
+		System.out.println();  
+        System.out.println("Array in reverse order: ");    
+        for (int i = arr.length-1; i >= 0; i--) {  
+            System.out.print(arr[i] + " ");  
+
+}
+}
+}
