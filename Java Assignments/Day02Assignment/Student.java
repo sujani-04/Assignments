77bffb2e7b1d360d81509d0195f6e9ce7c24374d
sujani-04 <94522136+sujani-04@users.noreply.github.com>
2021-12-06 08:39:20 +0530

    Update Student.java

--- Java Assignments/Day02Assignment/Student.java
+++ Java Assignments/Day02Assignment/Student.java
@@ -1,1 +1,42 @@
+package com.sonata;
+
+public class Student {
+	int stdid;
+	String stdname;
+	int stdClass;
+	
+	Student(int n, String name, int c){
+		this.stdid = n;
+		this.stdname= name;
+		this.stdClass=c;
+		}
+		Student(){}
+	
+	public void display()
+	{
+		System.out.println(stdid);
+		System.out.println(stdname);
+		System.out.println(stdClass);
+	}
+	
+	public static void main(String args[])
+	{
+		Student s1 =new Student();
+		s1.stdid=101;
+		s1.stdname="Lucky";
+		s1.stdClass=9;
+
+		s1.display();
+		
+		Student s2 = new Student(102,"Laya",8);
+
+		s2.display();
+		
+		
+		
+	}
+
+
+
+}
 
