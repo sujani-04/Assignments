c8bc51ff1fe2db282adda1af545dad32b3838df8
sujani-04 <94522136+sujani-04@users.noreply.github.com>
2021-12-07 08:47:49 +0530

    Update Employee.java

--- Java Assignments/Day03Assignment/Employee.java
+++ Java Assignments/Day03Assignment/Employee.java
@@ -1,1 +1,18 @@
+package com.sonata;
+
+public class Employee {
+	int empId;
+	String empName;
+	double empSal;
+	public void display(){
+		System.out.println(empId);
+		System.out.println(empName);
+		System.out.println(empSal);
+	}  
+	public void salCal(){
+		double total=empSal;
+		
+	} 
+
+}
 
